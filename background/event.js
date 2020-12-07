57019ce3a8ddf00411766e871101571ca956d7e3
Frank Cheng <ufo22940268@gmail.com>
2020-12-07 08:07:22 +0800

    Tidy code.

--- background/event.js
+++ background/event.js
@@ -2,7 +2,6 @@
  * Created by cc on 2020/12/6.
  */
 "use strict";
-let currentTab;
 chrome.runtime.onMessage.addListener(function (message) {
     const {url, branch, demoboxLink, demoboxDate} = message;
     const pr = new PullRequest(branch);
